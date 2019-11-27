86aa418254509e392d608ebdda5add9adcf55427
noob-wale <wallacesheundah@gmail.com>
2019-11-27 10:49:25 +0300

    update color on clock hand

--- CSS+JS_Clock/app.js
+++ CSS+JS_Clock/app.js
@@ -14,9 +14,11 @@ function setDate() {
 
     const minuteDegree = ((minutes / 60) * 360) + 90;
     minHand.style.transform = `rotate(${minuteDegree}deg)`;
+    minHand.style.background = "red";
 
     const hourDegree = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegree}deg)`;
+    hourHand.style.background = "rgba(191, 207, 43, 0.74)"
 };
 
 setInterval(setDate, 1000);
